3dea3576b61f25662dce6aac9567dc5396445c32
Norman Jäckel <contact@normanjaeckel.de>
2024-03-28 13:42:09 +0100

    Run black and isort (coding style tool).

--- hoursinrestaurant/asgi.py
+++ hoursinrestaurant/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoursinrestaurant.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoursinrestaurant.settings")
 
 application = get_asgi_application()
