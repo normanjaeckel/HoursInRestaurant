eaf34aa04baf7d49eb815bba81ab2eff1961d8c7
Norman Jäckel <contact@normanjaeckel.de>
2024-08-15 21:36:01 +0200

    Fixed sorting

--- hoursinrestaurant/views.py
+++ hoursinrestaurant/views.py
@@ -23,5 +23,5 @@ class Overview(LoginRequiredMixin, TemplateView):
             for re in Restaurant.objects.all():
                 days[day]["staffs"].extend(re.staff_at_date(days[day]["date_obj"]))
 
-        context["days"] = sorted(days.items(), key=lambda t: t[0])
+        context["days"] = sorted(days.items(), key=lambda t: t[1]["date_obj"])
         return context
